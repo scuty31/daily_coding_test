a79db3eeb2e6e017154cbe0323738fabc2182652
Scuty <87815827+scuty31@users.noreply.github.com>
2026-03-25 16:47:33 +0900

    [Gold I] Title: K번째 수, Time: 352 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -9,9 +9,9 @@ def solution():
     while left <= right:
         mid = (left+right)//2
 
-        cnt = 0
+        cnt = 0     # mid에 해당하는 숫자 앞에 존재하는 모든 수의 개수
         for i in range(1, n+1):
-            cnt += min(mid // i, n)
+            cnt += min(mid // i, n)     # i번째 행에서 mid 이하인 숫자의 개수, 각 행의 최대 원소의 개수 = n
 
         if cnt >= k:
             answer = mid
