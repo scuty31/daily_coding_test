2f72cce1a09cbc9517235351953044c404cf5559
scuty31 <hyunssi31@gmail.com>
2026-07-11 11:58:19 +0900

    2026.07.11 daily coding test complete.

--- 프로그래머스/1/실패율.py
+++ 프로그래머스/1/실패율.py
@@ -1,18 +1,21 @@
 def solution(N, stages):
     answer = []
 
-    players = {i: 0 for i in range(N + 1)}
+    players = [0 for _ in range(N+2)]
 
     for level in stages:
-        for i in range(level):
-            players[i] += 1
+        players[level] += 1
 
     pre_players_cnt = 0
     fail = [(0, i) for i in range(N + 1)]
 
     for level in range(1, N + 1):
         all_player = len(stages) - pre_players_cnt
-        fail_player = all_player - players[level]
+        if all_player == 0:
+            fail[level] = (0, level)
+            continue
+
+        fail_player = players[level]
         fail[level] = (fail_player / all_player, level)
 
         pre_players_cnt += fail_player
