91d92fc37adbd2197febabe288aed2d7d968fd2c
scuty31 <hyunssi31@gmail.com>
2026-07-20 13:02:13 +0900

    2026-07-20 daily coding test complete

---
+++ 프로그래머스/3/N으로 표현.py
@@ -0,0 +1,39 @@
+def solution(N, number):
+    answer = -1
+
+    if N == number:
+        return 1
+
+    # 각 개수를 사용해 만들 수 있는 숫자들
+    dp = [set() for _ in range(9)]
+    dp[1].add(N)
+
+    for i in range(2, len(dp)):
+        dp[i].add(int(str(N)*i))
+
+        for j in range(1, i):
+            for n in dp[j]:
+                for m in dp[i-j]:
+                    if n + m >= 0:
+                        dp[i].add(n + m)
+                    if n - m >= 0:
+                        dp[i].add(n - m)
+                    if m - n >= 0:
+                        dp[i].add(m - n)
+                    dp[i].add(n*m)
+                    if m != 0:
+                        dp[i].add((-n)//(-m))
+                    if n != 0:
+                        dp[i].add((-m)//(-n))
+
+        if number in dp[i]:
+            answer = i
+            break
+
+    return answer
+
+
+N_input = 3
+number_input = 170
+
+print(solution(N_input, number_input))
