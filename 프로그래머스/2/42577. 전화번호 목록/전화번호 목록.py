41a6f03fc1c9f080d57f8a20439be3fb44ce9789
Scuty <87815827+scuty31@users.noreply.github.com>
2026-07-21 10:06:54 +0900

    [level 2] Title: 전화번호 목록, Time: 456.03 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,14 @@
 def solution(phone_book):
-    phone_dict = {i:1 for i in phone_book}
+    answer = True
+    phone_dict = dict(zip(phone_book, [1 for _ in range(len(phone_book))]))
 
     for phone in phone_book:
         phone_list = list(phone)
+        phone_num = ''
 
         for i in range(len(phone_list)-1):
-            if ''.join(phone_list[:i+1]) in phone_dict:
+            phone_num += phone_list[i]
+            if phone_dict.get(phone_num):
                 return False
 
-    return True
+    return answer
