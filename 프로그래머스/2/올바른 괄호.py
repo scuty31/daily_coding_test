9d2db790531351a9a9eac49f2b93a542da6ed3bf
scuty31 <hyunssi31@gmail.com>
2026-07-21 11:29:21 +0900

    2026.07.21 daily coding test complete.

---
+++ 프로그래머스/2/올바른 괄호.py
@@ -0,0 +1,20 @@
+def solution(s):
+    stack = []
+    for i in range(len(s)):
+        op = s[i]
+        if op == '(':
+            stack.append(op)
+        else:
+            if len(stack) > 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                return False
+
+    if stack:
+        return False
+
+    return True
+
+
+s_input = ")()("
+print(solution(s_input))
