cb55f58092a8cfe3d7f03a715e4f485ca88e2bf6
Scuty <87815827+scuty31@users.noreply.github.com>
2026-07-22 14:31:01 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 4.80 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -0,0 +1,37 @@
+def solution(message, spoiler_ranges):
+    answer = 0
+    message_idx = [0 for _ in range(len(message))]
+    message_dict = dict()
+
+    idx = 1
+
+    for start, end in spoiler_ranges:
+        for i in range(start, end+1):
+            message_idx[i] = idx
+
+        idx += 1
+
+    words = set()
+    message_list = message.split()
+
+    for m in message_list:
+        message_dict[m] = message_list.count(m)
+
+    idx = 0
+
+    for i in range(len(message)):
+        if message[i] == ' ':
+            idx += 1
+
+        elif message_idx[i] > 0:
+            words.add((message_list[idx], idx))
+
+    word_list = []
+    for word, _ in words:
+        word_list.append(word)
+
+    for word in set(word_list):
+        if word_list.count(word) == message_dict[word]:
+            answer += 1
+
+    return answer
