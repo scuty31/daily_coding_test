7625ebd360bc6ce9b04b827dc8a2d78bc3e1753f
scuty31 <hyunssi31@gmail.com>
2026-07-24 12:44:02 +0900

    2026.07.24 daily coding test complete.

---
+++ 프로그래머스/2/2개 이하로 다른 비트.py
@@ -0,0 +1,47 @@
+def change_bit(n):
+    bit_num = []
+
+    while n:
+        bit_num.append(n % 2)
+        n //= 2
+
+    if bit_num.count(0) == 0:
+        bit_num.append(0)
+
+    return bit_num
+
+
+def change_dec(bit_n):
+    num = 0
+
+    for i in range(len(bit_n)):
+        num += bit_n[i] * (2**i)
+
+    return num
+
+
+def solution(numbers):
+    answer = []
+
+    for n in numbers:
+        bit_n = change_bit(n)
+
+        if n % 2 == 1:
+            for i in range(len(bit_n)):
+                if bit_n[i] == 0:
+                    bit_n[i] = 1
+                    bit_n[i-1] = 0
+                    break
+
+        else:
+            for i in range(len(bit_n)):
+                if bit_n[i] == 0:
+                    bit_n[i] = 1
+                    break
+
+        answer.append(change_dec(bit_n))
+
+    return answer
+
+numbers_input = [2, 7, 5]
+print(solution(numbers_input))
